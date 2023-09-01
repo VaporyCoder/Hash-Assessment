90303b7630d47b442194401044bfa2c133200d50
Jon <jlara2019@gmail.comgit config --global user.email jlara2019@gmail.compwdgit commit>
2023-09-01 14:09:43 -0500

    still working

--- app.js
+++ app.js
@@ -1,28 +1,59 @@
+const playerList = document.querySelector("ul");
+//^^ This is where the player list will be displayed ^^
+const detailDiv = document.querySelector("div");
+//^^ This is where I want to display the puppy's stats ^^
+
 let players;
 
-const listDiv = document.querySelector('div');
-
-async function getPuppyList(){
-    const list = await fetch('https://fsa-puppy-bowl.herokuapp.com/api/2307/players');
-    const json = await list.json();
-    const players = json.data;
-    console.log(players)
+const getList = async () => {
+  const list = await fetch(
+    "https://fsa-puppy-bowl.herokuapp.com/api/2307/players"
+  );
+  const json = await list.json();
+  players = json.data.players;
+   //^^ I have got my players and their data ^^
+ render();
 };
 
-function renderPuppyList(){
-    console.log(window.location.hash)
-    const allPuppies = state.puppyList.map((puppy) =>{
-        return `<div> <a href=#${puppy.name}> ${puppy.name} <a> </div>`
-    })
-    listDiv.innerHTML = allPuppies.join('')
-};
+const render = ()=> {
+  const hash = window.location.hash.slice(1)*1;
+  //^^ This is where I grab the hash from the link and then turn it to a number ^^
+  const html = players.map(puppy => {
+    return `
+    <li>
+    <a href='#${puppy.id}'>
+      ${puppy.name}
+      </a>
+    </li>
+    `;
+  }).join('');
+  //^^ This is where I mapped through players[] and made them into list items and then links ^^
+  playerList.innerHTML = html
+  //^^ This puts the puppy names onto the html page ^^
 
+  const puppy = players.find(puppy=> {
+    return puppy.id === hash;
+  });
+  //^^ If puppy id matches the hash then proceed with function ^^
 
-console.log(state);
-
-async function render(){
-    await getPuppyList();
-    renderPuppyList();
+  let detailDivHTML = 'The NPFL 2023-24 Season'
+  if(puppy) {
+  detailDivHTML = `
+  <img src='${puppy.imageUrl}' style='height: 50vh'/>
+  </br>
+  ${puppy.name}
+  </br>
+  ${puppy.breed}
+  </br>
+  ${puppy.status}
+  `;
+  }
+  detailDiv.innerHTML = detailDivHTML
+  //^^ Grabbing the puppy stats and putting them on the HTML page ^^
 }
 
-render();
+window.addEventListener('hashchange', ()=> {
+  render()
+});
+
+getList();
