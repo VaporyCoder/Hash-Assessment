64a354c795f3ffdb32c8605eb206d1672c7f0d6b
Jonathan Lara <vaporycoder@Jonathans-MacBook-Pro.local>
2023-09-02 15:10:04 -0500

    still working

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const playerList = document.querySelector("ul");
+const playerList = document.querySelector("nav");
 //^^ This is where the player list will be displayed ^^
 const detailDiv = document.querySelector(".card");
 //^^ This is where I want to display the puppy's stats ^^
@@ -11,49 +11,48 @@ const getList = async () => {
   );
   const json = await list.json();
   players = json.data.players;
-   //^^ I have got my players and their data ^^
- render();
+  //^^ I have got my players and their data ^^
+  render();
 };
 
-const render = ()=> {
-  const hash = window.location.hash.slice(1)*1;
+const render = () => {
+  const hash = window.location.hash.slice(1) * 1;
   //^^ This is where I grab the hash from the link and then turn it to a number ^^
-  const html = players.map(puppy => {
-    return `
+  const html = players
+    .map((puppy) => {
+      return `
     <li>
-    <a href='#${puppy.id}'>
+    <a href='#${puppy.id}'> <button class='btn'>
       ${puppy.name}
-      </a>
+      </button> </a>
     </li>
     `;
-  }).join('');
+    })
+    .join("");
   //^^ This is where I mapped through players[] and made them into list items and then links ^^
-  playerList.innerHTML = html
+  playerList.innerHTML = html;
   //^^ This puts the puppy names onto the html page ^^
 
-  const puppy = players.find(puppy=> {
+  const puppy = players.find((puppy) => {
     return puppy.id === hash;
   });
   //^^ If puppy id matches the hash then proceed with function ^^
 
-  let detailDivHTML = 'The NPFL 2023-24 Season'
-  if(puppy) {
-  detailDivHTML = `
-  <img src='${puppy.imageUrl}'/>
-  </br>
-  ${puppy.name}
-  </br>
-  ${puppy.breed}
-  </br>
-  ${puppy.status}
+  let detailDivHTML = "The NPFL 2023-24 Season";
+  if (puppy) {
+    detailDivHTML = `
+  <img class='playerPic' src='${puppy.imageUrl}' style=' border-radius: 15px'/>
+  <h2>${puppy.name}</h2>
+  <p>${puppy.breed}</p>
+  <p>${puppy.status}</p>
   `;
   }
-  detailDiv.innerHTML = detailDivHTML
+  detailDiv.innerHTML = detailDivHTML;
   //^^ Grabbing the puppy stats and putting them on the HTML page ^^
-}
+};
 
-window.addEventListener('hashchange', ()=> {
-  render()
+window.addEventListener("hashchange", () => {
+  render();
 });
 
 getList();
